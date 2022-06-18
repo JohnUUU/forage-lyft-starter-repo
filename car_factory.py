bcc3153e05736c424cddde580c5771819a696230
John UUU <john_w_wu@brown.edu>
2022-06-18 12:13:32 -0500

    Made the car factory methods static
    The car factory methods have no dependence on the Car Factory instance and is public so they should be static methods.

--- car_factory.py
+++ car_factory.py
@@ -8,27 +8,32 @@ from car import Car
 
 
 class CarFactory():
-    def create_calliope(self, current_date, last_service_date, current_mileage, last_service_mileage):
+    @staticmethod
+    def create_calliope(current_date, last_service_date, current_mileage, last_service_mileage):
         engine = CapuletEngine(current_mileage, last_service_mileage)
         battery = SpindlerBattery(current_date, last_service_date)
         return Car(engine, battery)
 
-    def create_glissade(self, current_date, last_service_date, current_mileage, last_service_mileage):
+    @staticmethod
+    def create_glissade(current_date, last_service_date, current_mileage, last_service_mileage):
         engine = WilloughbyEngine(current_mileage, last_service_mileage)
         battery = SpindlerBattery(current_date, last_service_date)
         return Car(engine, battery)
 
-    def create_sternman(self, current_date, last_service_date, warning_light_is_on):
+    @staticmethod
+    def create_sternman(current_date, last_service_date, warning_light_is_on):
         engine = SternmanEngine(warning_light_is_on)
         battery = NubbinBattery(current_date, last_service_date)
         return Car(engine, battery)
 
-    def create_rorschach(self, current_date, last_service_date, current_mileage, last_service_mileage):
+    @staticmethod
+    def create_rorschach(current_date, last_service_date, current_mileage, last_service_mileage):
         engine = WilloughbyEngine(current_mileage, last_service_mileage)
         battery = NubbinBattery(current_date, last_service_date)
         return Car(engine, battery)
 
-    def create_thovex(self, current_date, last_service_date, current_mileage, last_service_mileage):
+    @staticmethod
+    def create_thovex(current_date, last_service_date, current_mileage, last_service_mileage):
         engine = CapuletEngine(current_mileage, last_service_mileage)
         battery = NubbinBattery(current_date, last_service_date)
         return Car(engine, battery)
